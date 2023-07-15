fa4c4f04b6206041f43b17265a6c91f9b332c50f
Omar Benavides <omar.benavides.rmz@gmail.com>
2023-07-15 14:10:44 -0500

    chore: Add util to shuffle an array

--- src/common/lib/utils.ts
+++ src/common/lib/utils.ts
@@ -1,1 +1,18 @@
 export const randomRange = (min: number, max: number) => Math.random() * (max - min) + min;
+
+export const shuffleArray = (arr: any[]) => {
+  let currentIndex = arr.length;
+  let randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [arr[currentIndex], arr[randomIndex]] = [arr[randomIndex], arr[currentIndex]];
+  }
+
+  return arr;
+};
