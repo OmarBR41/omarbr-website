f4ca189005746f1c61381991916b46ad9969814a
Omar Benavides <omar.benavides.rmz@gmail.com>
2023-07-15 14:11:06 -0500

    chore: Add util to convert a string to a slug

--- src/common/lib/utils.ts
+++ src/common/lib/utils.ts
@@ -16,3 +16,11 @@ export const shuffleArray = (arr: any[]) => {
 
   return arr;
 };
+
+export const slugify = (str: string) =>
+  str
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, '')
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '');
