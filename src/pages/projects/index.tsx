204dd2f907a3ddd6a86a87a98609cf02ed180d57
Omar Benavides <omar.benavides.rmz@gmail.com>
2023-07-15 14:18:32 -0500

    projects: Add index page

---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,30 @@
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import { ContactSection, SocialMedia } from '@/modules/contact';
+import { Head } from '@/modules/layout';
+import { ProjectsSection } from '@/modules/projects';
+
+const ProjectsIndexPage = () => {
+  const { t } = useTranslation('projects');
+  const PAGE_TITLE = t('title');
+
+  return (
+    <>
+      <Head title={PAGE_TITLE} />
+      <ProjectsSection />
+      <ContactSection />
+      <SocialMedia />
+    </>
+  );
+};
+
+export async function getStaticProps({ locale }: { locale: string }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'contact', 'projects'])),
+    },
+  };
+}
+
+export default ProjectsIndexPage;
