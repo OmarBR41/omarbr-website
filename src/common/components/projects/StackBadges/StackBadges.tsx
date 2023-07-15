8e56a771f9b4c5ae53e8e47dd660070172971f41
Omar Benavides <omar.benavides.rmz@gmail.com>
2023-07-15 14:03:51 -0500

    projects: Add component to render a stack's shield badges

---
+++ src/common/components/projects/StackBadges/StackBadges.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import styles from './StackBadges.module.css';
+
+export type Badge =
+  | 'html5'
+  | 'css3'
+  | 'javascript'
+  | 'typescript'
+  | 'react'
+  | 'redux'
+  | 'nextjs'
+  | 'nodejs'
+  | 'expressjs'
+  | 'mui'
+  | 'tailwindcss'
+  | 'postgresql'
+  | 'mongodb'
+  | 'graphql'
+  | 'netlify';
+
+interface StackBadgesProps {
+  id: string;
+  stack: Badge[];
+}
+
+const StackBadges = ({ id, stack }: StackBadgesProps) => {
+  return (
+    <div className={styles.container}>
+      {stack.map((name) => (
+        <div key={`${id}-${name}`} className={styles.badgeContainer}>
+          <img className={styles.badgeImage} src={`/badges/${name}.svg`} alt={`${name} badge`} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export { StackBadges };
---
+++ src/common/components/projects/StackBadges/index.ts
@@ -0,0 +1,1 @@
+export { type Badge, StackBadges } from './StackBadges';
