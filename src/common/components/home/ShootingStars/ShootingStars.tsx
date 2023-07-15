5818fbea59a6733a2cf27d1ba5cd5f362a1ee21b
Omar Benavides <omar.benavides.rmz@gmail.com>
2023-07-15 02:41:23 -0500

    home: Fix random shooting stars from Hero bg

--- src/common/components/home/ShootingStars/ShootingStars.tsx
+++ src/common/components/home/ShootingStars/ShootingStars.tsx
@@ -7,7 +7,7 @@ import styles from './ShootingStars.module.css';
 const NUM_OF_STARS = 8;
 
 export const ShootingStars = () => {
-  const renderStars = () => Array.from(Array(NUM_OF_STARS).keys()).map((e) => <Star key={e} />);
+  const renderStars = () => Array.from(Array(NUM_OF_STARS).keys()).map((e: number) => <Star key={`star-${e}`} />);
 
   return (
     <div className={styles.container}>
--- src/common/components/home/ShootingStars/Star/Star.tsx
+++ src/common/components/home/ShootingStars/Star/Star.tsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { randomRange } from '@/lib/utils';
 
 import styles from './Star.module.css';
 
+const DEFAULT_STATE = {
+  animationDelay: 8000,
+  opacity: 0.8,
+  posX: 50,
+  posY: 50,
+};
+
+interface StarState {
+  animationDelay: number;
+  opacity: number;
+  posX: number | string;
+  posY: number | string;
+}
+
 export const Star = () => {
-  const positionY = `calc(${Math.random() * 50}% - ${Math.random() * 400 - 200}px)`;
-  const positionX = `calc(${Math.random() * 50}% - ${Math.random() * 300}px)`;
-  const animationDelay = `${Math.random() * 10000}ms`;
+  const [state, setState] = useState<StarState>(DEFAULT_STATE);
+
+  useEffect(() => {
+    const animationDelay = Math.random() * 10000;
+
+    const posY = `calc(${Math.random() * 50}% - ${Math.random() * 400 - 200}px)`;
+    const posX = `calc(${Math.random() * 50}% - ${Math.random() * 300}px)`;
+    const opacity = randomRange(0.4, 1);
+
+    setState({
+      animationDelay,
+      opacity,
+      posX,
+      posY,
+    });
+  }, []);
 
   return (
     <div
       className={styles.container}
       style={{
-        top: positionY,
-        left: positionX,
-        animationDelay,
+        top: state.posY,
+        left: state.posX,
+        animationDelay: `${state.animationDelay}ms`,
+        opacity: state.opacity,
       }}
     />
   );
