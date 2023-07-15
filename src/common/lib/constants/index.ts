f94fc42328b4891b72d7f2a98a8243ccac7310c0
Omar Benavides <omar.benavides.rmz@gmail.com>
2023-07-15 14:20:00 -0500

    about: Add jobs data, assets & locales

--- src/common/lib/constants/index.ts
+++ src/common/lib/constants/index.ts
@@ -2,3 +2,4 @@ export * from './environment';
 export * from './navData';
 export * from './projects';
 export * from './screenBreakpoints';
+export * from './jobs';
---
+++ src/common/lib/constants/jobs.tsx
@@ -0,0 +1,42 @@
+export const WORK_EXPERIENCE = [
+  {
+    companyName: 'Evernote',
+    companyLogoUrl: '/jobs/evernote.svg',
+    jobTitle: 'software-engineer',
+    jobOverview:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    startDate: '03/2022',
+    endDate: '02/2023',
+    stack: ['react', 'typescript'],
+  },
+  {
+    companyName: 'Lernit',
+    companyLogoUrl: '/jobs/lernit.png',
+    jobTitle: 'fullstack-developer',
+    jobOverview:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    startDate: '02/2021',
+    endDate: '01/2022',
+    stack: ['react', 'typescript'],
+  },
+  {
+    companyName: 'Green Clean',
+    companyLogoUrl: '/jobs/green-clean.png',
+    jobTitle: 'fullstack-developer',
+    jobOverview:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    startDate: '02/2020',
+    endDate: '11/2020',
+    stack: ['react', 'typescript'],
+  },
+  {
+    companyName: 'Besser Lighting',
+    companyLogoUrl: '/jobs/besser-lighting.png',
+    jobTitle: 'web-developer',
+    jobOverview:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    startDate: '08/2017',
+    endDate: '12/2019',
+    stack: ['react', 'typescript'],
+  },
+];
