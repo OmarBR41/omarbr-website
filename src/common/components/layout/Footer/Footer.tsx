91f8596628f535bc092248b14bea517ed35c5a8d
Omar Benavides <omar.benavides.rmz@gmail.com>
2023-07-18 23:43:53 -0500

    footer: Update Footer

--- src/common/components/layout/Footer/Footer.tsx
+++ src/common/components/layout/Footer/Footer.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
+import { Copyright, NavGroup } from '@/components/footer';
+import { Logo } from '@/components/ui';
+
+import { FOOTER_NAV_LEFT, FOOTER_NAV_RIGHT } from '@/lib/constants';
 
 import styles from './Footer.module.css';
 
 export const Footer: React.FC = () => (
   <footer className={styles.container}>
+    <div className={styles.logoContainer}>
+      <Logo />
+    </div>
+
     <nav className={styles.nav}>
-      <p>Footer1</p>
-      <p>Footer2</p>
+      <NavGroup items={FOOTER_NAV_LEFT} />
+      <NavGroup items={FOOTER_NAV_RIGHT} />
     </nav>
+
+    <Copyright />
   </footer>
 );
