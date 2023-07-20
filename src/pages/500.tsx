dfea431d693a4228814699e8b58939c78fe2848b
Omar Benavides <omar.benavides.rmz@gmail.com>
2023-07-20 01:10:11 -0500

    analytics: Add 404 & 500 events

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,11 +1,25 @@
+import { useEffect } from 'react';
+
+import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
+import { event } from '@/config/analytics';
 import { ErrorLayout, Head } from '@/modules/layout';
 
 const Custom500 = () => {
+  const { asPath } = useRouter();
   const { t } = useTranslation('common');
   const PAGE_TITLE = t('errors.500.title');
+
+  useEffect(() => {
+    event({
+      action: 'Error',
+      category: 'Errors - 500',
+      label: asPath,
+    });
+  }, []);
+
   return (
     <>
       <Head title={PAGE_TITLE} />
