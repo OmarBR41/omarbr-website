5ae75798b89c9b8f1f644316a2e546cf3a7c913f
Omar Benavides <omar.benavides.rmz@gmail.com>
2023-07-19 23:53:37 -0500

    analytics: Add Hero events

--- src/modules/home/Hero/Hero.tsx
+++ src/modules/home/Hero/Hero.tsx
@@ -2,14 +2,30 @@ import React from 'react';
 
 import { useTranslation } from 'next-i18next';
 
-import { Divider } from '@/common/components/ui/Divider';
 import { ShootingStars, SkillsIcons } from '@/components/home';
 import { Button, ButtonVariant } from '@/components/ui/Button';
+import { Divider } from '@/components/ui/Divider';
+
+import { event } from '@/config/analytics';
 
 import styles from './Hero.module.css';
 
 export const Hero: React.FC = () => {
   const { t } = useTranslation('home');
+
+  const onPrimaryCTAClick = () => {
+    event({
+      action: 'Clicked Contact CTA',
+      category: 'Home - Hero',
+    });
+  };
+
+  const onSecondaryCTAClick = () => {
+    event({
+      action: 'Clicked Projects CTA',
+      category: 'Home - Hero',
+    });
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -25,10 +41,10 @@ export const Hero: React.FC = () => {
         <h2 className={styles.text}>{t('hero.text')}</h2>
 
         <div className={styles.ctaContainer}>
-          <Button href="/contact-me" variant={ButtonVariant.primary}>
+          <Button href="/contact-me" variant={ButtonVariant.primary} onClick={onPrimaryCTAClick}>
             {t('hero.cta-primary')}
           </Button>
-          <Button href="/projects" variant={ButtonVariant.secondary}>
+          <Button href="/projects" variant={ButtonVariant.secondary} onClick={onSecondaryCTAClick}>
             {t('hero.cta-secondary')}
           </Button>
         </div>
