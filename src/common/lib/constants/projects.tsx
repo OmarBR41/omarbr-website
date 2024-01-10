0a88b73c7aead52ee21a86871f36bb3f9d1d802e
Omar Benavides <omar.benavides.rmz@gmail.com>
2024-01-10 00:27:07 -0600

    Update projects

--- src/common/lib/constants/projects.tsx
+++ src/common/lib/constants/projects.tsx
@@ -17,66 +17,35 @@ export const PROJECTS: Project[] = [
       'graphql',
       'javascript',
       'typescript',
-      'mui',
+      'html5',
+      'tailwindcss',
       'postgresql',
-      // 'docker'
-      // 'digitalocean'
+      'docker',
+      'digital-ocean',
     ],
     // url: 'https://shop.wobs.gg',
     isInDevelopment: true,
   },
   {
     id: 'david-camporredondo',
-    stack: [
-      'react',
-      'nextjs',
-      'tailwindcss',
-      'javascript',
-      'typescript',
-      'html5',
-      // 'netlify'
-    ],
+    stack: ['react', 'nextjs', 'javascript', 'typescript', 'html5', 'tailwindcss'],
     url: 'https://davidcamporredondo.com',
   },
   {
     id: 'omarbr',
-    stack: [
-      'react',
-      'nextjs',
-      'css3',
-      'javascript',
-      'typescript',
-      'html5',
-      // 'digitalocean'
-    ],
+    stack: ['react', 'nextjs', 'javascript', 'typescript', 'html5', 'css3'],
     url: 'https://omarbr.com',
     repoUrl: 'https://github.com/OmarBR41/omarbr-website',
   },
   {
     id: 'auth-passport-jwt',
-    stack: [
-      'react',
-      'tailwindcss',
-      'redux',
-      'javascript',
-      'nodejs',
-      'expressjs',
-      'graphql',
-      'mongodb',
-      // 'netlify'
-    ],
+    stack: ['react', 'redux', 'javascript', 'nodejs', 'expressjs', 'graphql', 'mongodb', 'tailwindcss'],
     url: 'https://auth-passport-jwt.netlify.app',
     repoUrl: 'https://github.com/OmarBR41/auth-passport-jwt',
   },
   {
     id: 'ip-address-tracker',
-    stack: [
-      'react',
-      'html5',
-      'css3',
-      'javascript',
-      // 'netlify'
-    ],
+    stack: ['react', 'javascript', 'html5', 'css3'],
     url: 'https://ip-address-tracker-obr.netlify.app',
     repoUrl: 'https://github.com/OmarBR41/ip-address-tracker',
   },
